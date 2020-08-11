8299835988e3acf11a62600b07b5de884cd26a60
bpqvg <bpqvg@mail.ru>
2020-08-11 10:59:47 +0300

    Server: Fix cli client. Parse allinfo from all pc in local network.

--- server/cli-client/client.py
+++ server/cli-client/client.py
@@ -12,11 +12,11 @@ utils_libs_dir_path = current_dir_path+"/../../utils/"
 maplocalnetwork = imp.load_source('maplocalnetwork', utils_libs_dir_path+"maplocalnetwork.py")
 
 class Client:
-    # def __init__(self):
-    #     self.local_maplocalnetwork = maplocalnetwork.MapLocalNetwork()
-    #     self.local_ips = self.local_maplocalnetwork.map_network()
-    # def get_local_ips(self):
-    #     return self.local_ips
+    def __init__(self):
+        self.local_maplocalnetwork = maplocalnetwork.MapLocalNetwork()
+        self.local_ips = self.local_maplocalnetwork.map_network()
+    def get_local_ips(self):
+        return self.local_ips
     def get_all_info_by_one_machine(self, job_q, results_q):
         while True:
             ip = job_q.get()
@@ -39,17 +39,16 @@ class Client:
         jobs = multiprocessing.Queue()
         results = multiprocessing.Queue()
 
-        # pool_size=len(self.local_ips)+1
-        pool_size=255
+        pool_size=len(self.local_ips)+1
+        # pool_size=255
         pool = [multiprocessing.Process(target=self.get_all_info_by_one_machine, args=(jobs, results)) for i in range(pool_size)]
 
         for p in pool:
             p.start()
         # cue hte ping processes
         jobs.put('192.168.21.89') # Необходимо также проверить собственный адрес
-        # jobs.put('192.168.21.82')
-        # for ip in self.local_ips:
-        #     jobs.put(ip)
+        for ip in self.local_ips:
+            jobs.put(ip)
         
         for p in pool:
             jobs.put(None)
@@ -64,6 +63,6 @@ class Client:
 
         return json.dumps(allinfo_list)
 
-a = Client()
-b = a.get_all_info_from_all_machines_in_local_network()
-print(b)
+# a = Client()
+# b = a.get_all_info_from_all_machines_in_local_network()
+# print(b)
